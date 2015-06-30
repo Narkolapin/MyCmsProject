d3e17ab112ae159342e4fc9af2223ea2f6b8a72b
narkolpain <narkolapin@gmail.com>
2015-06-30 09:08:43 +0200

    Fichier de base de données MySQL

---
+++ cms/model/rev/cms.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 30 Juin 2015 à 09:06
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sectiona`
+--
+
+CREATE TABLE IF NOT EXISTS `sectiona` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `Text` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
